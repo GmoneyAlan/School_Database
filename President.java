e909fb04860e72b4f57ff93a3ac7afb5663a9691
GmoneyAlan <alan200@outlook.com>
2020-10-03 20:29:06 -0600

    Added to Pres

--- President.java
+++ President.java
@@ -1,28 +1,35 @@
+import java.util.Random;
+import java.util.Hashtable;
+
 public class President extends Faculty {
     //Professor list
-
+    private Hashtable<String, Professor> professors = new Hashtable<String, Professor>();
+    private Random random = new Random();
+    private int setClassCost = 500;
+    
     President(boolean override, int id, boolean isPres, String name){
         super(override, id, isPres, name);
     }
 
-    public void changeTuition(){
-
+    public void changeClassTuition(Classroom classroom, int cost){
+        classroom.setCost(Cost);
     }
 
     public void fireProfessor(){
-
+        
     }
 
-    public void hireProfessor(){
-
+    public void hireProfessor(String name){
+        int id = random.nextInt(999999)
+        professors.put(name, new Professor(true, id, false, name))
     }
 
     public void dropDecision(){
 
     }
 
-    public void newClass(){
-
+    public void newClass(School school, Professor professor){
+        school.addClass(new Classroom(professor, "", 0, 1, setClassCost);
     }
 
     public void deleteClass(){
