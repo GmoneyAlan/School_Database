20fd5b3184aa05eabf3affbc3b9d5dbb1211210b
unknown <brianrnovoa@gmail.com>
2020-10-03 19:55:57 -0600

    push

---
+++ Professor.java
@@ -0,0 +1,9 @@
+public class Professor extends Faculty {
+    
+    Professor(boolean override, int id, boolean isPres, String name) {
+        super(override, id, isPres, name);
+    }
+
+    
+    
+}
